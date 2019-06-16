16cbdfbb2ee3b57744a7a3437461d9f38dc747d5
Gabriel Ravier <gabravier@gmail.com>
2019-06-16 14:01:00 +0200

    Added RISC-V version of a.out.asm
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ RISCV-32/a.out.asm
@@ -0,0 +1,60 @@
+N_MAGIC:
+	lhu a0, 0(a0)
+	ret
+
+
+
+
+
+N_MACHTYPE:
+	lbu a0, 2(a0)
+	ret
+
+
+
+
+
+N_FLAGS:
+	lbu a0, 3(a0)
+	ret
+
+
+
+
+
+N_SET_INFO:
+	slli a2, 16
+	or a1, a2
+	slli a2, a3, 24
+	or a1, a2
+	sw a1, 0(a0)
+	ret
+
+
+
+
+
+N_BADMAG:
+	mv a1, a0
+	li a0, 0
+
+	lhu a1, 0(a1)
+	addi a2, a1, -263
+	li a3, 4
+	bltu a3, a2, .almostThere
+
+	li a3, 1
+	sll a2, a3, a2
+	andi a2, 0x13
+	bnez a2, .return
+
+.almostThere:
+	li a2, 0xCC
+	bne a1, a2, .ret1
+
+.return:
+	ret
+
+.ret1:
+	li a0, 1
+	ret
