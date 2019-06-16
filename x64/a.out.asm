dfb206f0087bdb08566cfe6b314da1c4c925cdd9
Gabriel Ravier <gabravier@gmail.com>
2019-06-16 12:23:12 +0200

    Finished x64/a.out.asm
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- x64/a.out.asm
+++ x64/a.out.asm
@@ -1,5 +1,8 @@
 global N_MAGIC
 global N_MACHTYPE
+global N_FLAGS
+global N_SET_INFO
+global N_BADMAG
 
 section .text align=16
 
@@ -15,4 +18,56 @@ N_MAGIC:
 	align 16
 N_MACHTYPE:
 	mov al, [rdi + 2]
+	ret
+
+
+
+
+
+	align 16
+N_FLAGS:
+	mov al, [rdi + 3]
+	ret
+
+
+
+
+
+	align 16
+N_SET_INFO:
+	movzx edx, dl
+	movzx esi, si
+	sal ecx, 24
+	sal edx, 16
+	or esi, edx
+	or esi, ecx
+	movsx rsi, esi
+	mov [rdi], rsi
+	ret
+
+
+
+
+
+	align 16
+N_BADMAG:
+	movzx rcx, word [rdi]
+	lea rax, [rcx - 263]
+
+	cmp rax, 1
+	seta dl
+
+	cmp rcx, 267
+	setne al
+
+	xor esi, esi
+	test dl, al
+	je .retEsi
+
+	xor esi, esi
+	cmp rcx, 204
+	setne sil
+
+.retEsi:
+	mov eax, esi
 	ret
