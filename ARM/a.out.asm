ec589291d188971d66c52e102a84f41fad5ef7aa
Gabriel Ravier <gabravier@gmail.com>
2019-06-16 13:08:10 +0200

    Optimized N_BADMAG and tweaked N_SET_INFO
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- ARM/a.out.asm
+++ ARM/a.out.asm
@@ -25,7 +25,7 @@ N_FLAGS:
 N_SET_INFO:
 	orr r2, r1, r2, asl #16
 	orr r3, r2, r3, asl #24
-	str r3, [r0, #0]
+	str r3, [r0]
 	bx lr
 
 
@@ -35,18 +35,17 @@ N_SET_INFO:
 N_BADMAG:
 	ldrh r3, [r0]
 	ldr r2, .dat
-
 	sub r0, r3, #260
-	sub r0, #3
 
-	cmp r3, r2
-	cmpne r0, #1
-	movls r0, #0
-	movhi r0, #1
-	bxls lr
-
-	subs r0, r3, #204
+	cmp r0, r2
+	cmpne r0, #204
 	movne r0, #1
+	moveq r0, #0
+	sub r3, #3
+
+	cmp r3, #1
+	movls r0, #0
+	andhi r0, #1
 	bx lr
 
 .dat:
