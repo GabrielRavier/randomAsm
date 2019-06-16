3defd93e21002109ab14cdf55c56551d5e29e300
Gabriel Ravier <gabravier@gmail.com>
2019-06-16 13:15:44 +0200

    Added ARM64 a.out.asm
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ ARM64/a.out.asm
@@ -0,0 +1,47 @@
+N_MAGIC:
+	ldrh w0, [x0]
+	ret
+
+
+
+
+
+N_MACHTYPE:
+	ldrb w0, [x0, 2]
+	ret
+
+
+
+
+
+N_FLAGS:
+	ldrb w0, [x0, 3]
+	ret
+
+
+
+
+
+N_SET_INFO:
+	uxth w1
+	ubfiz w2, 16, 0
+	orr w1, w2, w1
+	orr w3, w1, w3, lsl 24
+	sxtw x1, w3
+	str x1, [x0]
+	ret
+
+
+
+
+
+N_BADMAG:
+	ldrh w0, [x0]
+	mov x2, 20
+	cmp x0, 267
+	sub x1, x0, #263
+
+	ccmp x0, x2, 4, ne
+	ccmp x1, 1, 0, ne
+	cset w0, hi
+	ret
