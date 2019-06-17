a7b1c97332bdba7858edbf5aa75291a15ca04ca6
Gabriel Ravier <gabravier@gmail.com>
2019-06-17 16:57:44 +0200

    Added PowerPC and RISCV-64 bmi2intrin
    Also removed TI 83 CE from TODO (can't be done, nopie)
    Added 6502 to TODO (being done)
    
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ RISCV-64/bmi2intrin.asm
@@ -0,0 +1,22 @@
+_mulx_u64:
+	mulhu a5, a0, a1
+	mul a0, a1
+	sd a5, 0(a2)
+	ret
+
+
+
+
+
+_mulx_u32:
+	slli a0, 32
+	slli a1, 32
+	srli a1, 32
+	srli a0, 32
+	mul a0, a1
+
+	srli a5, a0, 32
+	sw a5, 0(a2)
+
+	sext.w a0
+	ret
