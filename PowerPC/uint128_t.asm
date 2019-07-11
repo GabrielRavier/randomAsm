0fef08adbf3219183c60d4b525f8b4c155892d91
Gabriel Ravier <gabravier@gmail.com>
2019-07-11 22:51:06 +0200

    Continued PowerPC uint128_t
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- PowerPC/uint128_t.asm
+++ PowerPC/uint128_t.asm
@@ -265,6 +265,181 @@ uint128_t_operator_not:
 
 
 uint128_t_operator_shiftLeft:
+	mflr 0
+	stwu 1, -32(1)
+	stw 0, 36(1)
+	stw 31, 28(1)
+	mr 31, 3
+	stw 29, 20(1)
+	stw 30, 24(1)
+
+	lwz 8, 0(5)
+	lwz 9, 4(5)
+	lwz 10, 8(5)
+	lwz 11, 12(5)
+
+	or. 7, 8, 9
+	bne 0, .l2
+
+	cmpwi 7, 10, 0
+	bne 7, .l2
+
+	cmplwi 6, 11, 0x7F
+	bgt 6, .l2
+
+	cmpwi 6, 11, 0X40
+	beq 6, .l24
+
+	or. 9, 10, 11
+	beq 0, .l25
+	beq 7, .l26
+
+.l8:
+	addic 9, 11, -0x41
+	addme 8, 10
+
+	cmpwi 7, 8, 0
+	bne 7, .l2
+
+	cmplwi 7, 9, 0x3E
+	bgt 7, .l2
+
+	addic 9, 11, -0x60
+	addi 10, 11, -0x40
+	blt 0, .l17
+
+	lwz 10, 12(4)
+	slw 9, 10, 9
+	li 10, 0
+	stw 9, 0(31)
+	stw 10, 4(31)
+
+.l18:
+	lwz 0, 36(1)
+	li 10, 0
+	lwz 29, 20(1)
+	lwz 30, 24(1)
+	mr 3, 31
+
+	stw 10, 8(31)
+	stw 10, 12(31)
+
+	mtlr 0
+	lwz 31, 28(1)
+	addi 1, 0x20
+	blr
+
+.l2:
+	lis 4, uint128_0@ha
+	mr 3, 31
+	la 4, uint128_0@l(4)
+	bl uint128_t_constructor
+
+.l1:
+	lwz 0, 36(1)
+	mr 3, 31
+	lwz 29, 20(1)
+	lwz 30, 24(1)
+	lwz 31, 28(1)
+
+	addi 1, 0x20
+	mtlr 0
+	blr
+
+.l26:
+	cmplwi 7, 11, 0x3F
+	bgt 7, .l8
+
+	subfic 9, 11, 0x40
+	lwz 0, 8(4)
+	lwz 3, 12(4)
+	addic. 8, 9, -0x20
+	blt 0, .l10
+
+	srw 7, 0, 8
+	li 6, 0
+
+.l11:
+	addic. 5, 11, -0x20
+	blt 0, .l12
+
+	lwz 4, 4(4)
+	li 9, 0
+	slw 8, 4, 5
+
+.l13:
+	cmpwi 7, 5, 0
+	addc 9, 7
+	adde 8, 6
+	stw 8, 0(31)
+	stw 9, 4(31)
+	blt 7, .l14
+
+	slw 5, 4, 5
+	li 9, 0
+	stw 5, 8(31)
+	stw 9, 12(31)
+	b .l1
+
+.l24:
+	lwz 10, 8(4)
+	lwz 11, 12(4)
+	stw 10, 0(3)
+	stw 11, 4(3)
+
+	li 10, 0
+	stw 10, 8(3)
+	stw 10, 12(3)
+	b .l1
+
+.l25:
+	bl uint128_t_constructor
+	b .l1
+
+.l10:
+	slwi 8, 0, 1
+	subfic 5, 9, 0x1F
+	slw 8, 5
+	srw 7, 3, 9
+	or 7, 8, 7
+	srw 6, 0, 9
+	b .l11
+
+.l14:
+	srwi 10, 3, 1
+	subfic 9, 11, 0x1F
+	srw 9, 10, 9
+	slw 0, 11
+	or 0, 9
+	slw 3, 11
+	stw 0, 8(31)
+	stw 3, 12(31)
+	b .l1
+
+.l12:
+	lwz 12, 4(4)
+	lwz 30, 0(4)
+	subfic 4, 11, 0x1F
+	srwi 29, 12, 1
+	slw 8, 30, 11
+	srw 4, 29, 4
+	slw 9, 12, 11
+	or 8, 4, 8
+	b .l13
+
+.l17:
+	lwz 6, 12(4)
+	lwz 9, 8(4)
+	subfic 7, 10, 0x1F
+	mr 8, 6
+	srwi 6, 1
+	slw 9, 10
+	srw 7, 6, 7
+	or 9, 7
+	slw 10, 8, 10
+	stw 9, 0(31)
+	stw 10, 4(31)
+	b .l18
 
 
 
