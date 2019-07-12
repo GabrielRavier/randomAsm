2e95ff71189514d9beb70b2b9b613971ccff30bf
Gabriel Ravier <gabravier@gmail.com>
2019-07-12 19:59:16 +0200

    Continued PowerPC uint128_t
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- PowerPC/uint128_t.asm
+++ PowerPC/uint128_t.asm
@@ -279,34 +279,34 @@ uint128_t_operator_shiftLeft:
 	lwz 11, 12(5)
 
 	or. 7, 8, 9
-	bne 0, .l2
+	bne 0, .ret0
 
 	cmpwi 7, 10, 0
-	bne 7, .l2
+	bne 7, .ret0
 
 	cmplwi 6, 11, 0x7F
-	bgt 6, .l2
+	bgt 6, .ret0
 
 	cmpwi 6, 11, 0X40
-	beq 6, .l24
+	beq 6, .doLoadsStores
 
 	or. 9, 10, 11
-	beq 0, .l25
-	beq 7, .l26
+	beq 0, .doConstruct
+	beq 7, .check3F
 
-.l8:
+.doAdds:
 	addic 9, 11, -0x41
 	addme 8, 10
 
 	cmpwi 7, 8, 0
-	bne 7, .l2
+	bne 7, .ret0
 
 	cmplwi 7, 9, 0x3E
-	bgt 7, .l2
+	bgt 7, .ret0
 
 	addic 9, 11, -0x60
 	addi 10, 11, -0x40
-	blt 0, .l17
+	blt 0, .doShifts4
 
 	lwz 10, 12(4)
 	slw 9, 10, 9
@@ -314,7 +314,7 @@ uint128_t_operator_shiftLeft:
 	stw 9, 0(31)
 	stw 10, 4(31)
 
-.l18:
+.doLoads:
 	lwz 0, 36(1)
 	li 10, 0
 	lwz 29, 20(1)
@@ -329,13 +329,13 @@ uint128_t_operator_shiftLeft:
 	addi 1, 0x20
 	blr
 
-.l2:
+.ret0:
 	lis 4, uint128_0@ha
 	mr 3, 31
 	la 4, uint128_0@l(4)
 	bl uint128_t_constructor
 
-.l1:
+.return:
 	lwz 0, 36(1)
 	mr 3, 31
 	lwz 29, 20(1)
@@ -346,42 +346,42 @@ uint128_t_operator_shiftLeft:
 	mtlr 0
 	blr
 
-.l26:
+.check3F:
 	cmplwi 7, 11, 0x3F
-	bgt 7, .l8
+	bgt 7, .doAdds
 
 	subfic 9, 11, 0x40
 	lwz 0, 8(4)
 	lwz 3, 12(4)
 	addic. 8, 9, -0x20
-	blt 0, .l10
+	blt 0, .doShifts
 
 	srw 7, 0, 8
 	li 6, 0
 
-.l11:
+.doAdd:
 	addic. 5, 11, -0x20
-	blt 0, .l12
+	blt 0, .doShifts3
 
 	lwz 4, 4(4)
 	li 9, 0
 	slw 8, 4, 5
 
-.l13:
+.check0:
 	cmpwi 7, 5, 0
 	addc 9, 7
 	adde 8, 6
 	stw 8, 0(31)
 	stw 9, 4(31)
-	blt 7, .l14
+	blt 7, .doShifts2
 
 	slw 5, 4, 5
 	li 9, 0
 	stw 5, 8(31)
 	stw 9, 12(31)
-	b .l1
+	b .return
 
-.l24:
+.doLoadsStores:
 	lwz 10, 8(4)
 	lwz 11, 12(4)
 	stw 10, 0(3)
@@ -390,22 +390,22 @@ uint128_t_operator_shiftLeft:
 	li 10, 0
 	stw 10, 8(3)
 	stw 10, 12(3)
-	b .l1
+	b .return
 
-.l25:
+.doConstruct:
 	bl uint128_t_constructor
-	b .l1
+	b .return
 
-.l10:
+.doShifts:
 	slwi 8, 0, 1
 	subfic 5, 9, 0x1F
 	slw 8, 5
 	srw 7, 3, 9
 	or 7, 8, 7
 	srw 6, 0, 9
-	b .l11
+	b .doAdd
 
-.l14:
+.doShifts2:
 	srwi 10, 3, 1
 	subfic 9, 11, 0x1F
 	srw 9, 10, 9
@@ -414,9 +414,9 @@ uint128_t_operator_shiftLeft:
 	slw 3, 11
 	stw 0, 8(31)
 	stw 3, 12(31)
-	b .l1
+	b .return
 
-.l12:
+.doShifts3:
 	lwz 12, 4(4)
 	lwz 30, 0(4)
 	subfic 4, 11, 0x1F
@@ -425,9 +425,9 @@ uint128_t_operator_shiftLeft:
 	srw 4, 29, 4
 	slw 9, 12, 11
 	or 8, 4, 8
-	b .l13
+	b .check0
 
-.l17:
+.doShifts4:
 	lwz 6, 12(4)
 	lwz 9, 8(4)
 	subfic 7, 10, 0x1F
@@ -439,7 +439,7 @@ uint128_t_operator_shiftLeft:
 	slw 10, 8, 10
 	stw 9, 0(31)
 	stw 10, 4(31)
-	b .l18
+	b .doLoads
 
 
 
@@ -475,6 +475,183 @@ uint128_t_operator_shiftLeft_equal:
 
 
 uint128_t_operator_shiftRight:
+	mflr 0
+	stwu 1, -32(1)
+	stw 0, 36(1)
+	stw 31, 28(1)
+	mr 31, 3
+	stw 29, 20(1)
+	stw 30, 24(1)
+
+	lwz 8, 0(5)
+	lwz 9, 4(5)
+	lwz 10, 8(5)
+	lwz 11, 12(5)
+
+	or. 7, 8, 9
+	bne 0, .ret0
+
+	cmpwi 7, 10, 0
+	bne 7, .ret0
+
+	cmplwi 6, 11, 0x7F
+	bgt 6, .ret0
+
+	cmpwi 6, 11, 0X40
+	beq 6, .doHalf
+
+	or. 9, 10, 11
+	beq 0, .doConstruct
+	beq 7, .check3F
+
+.doAdds:
+	addic 9, 11, -0x41
+	addme 8, 10
+
+	cmpwi 7, 8, 0
+	bne 7, .ret0
+
+	cmplwi 7, 9, 0x3E
+	bgt 7, .ret0
+
+	addic 9, 11, -0x60
+	lwz 8, 0(4)
+	addi 10, 11, -0x40
+	blt 0, .doShifts4
+
+	li 10, 0
+	srw 9, 8, 9
+
+.doLoadsStores:
+	lwz 0, 36(1)
+	stw 10, 8(31)
+	li 11, 0
+	lwz 29, 20(1)
+	stw 9, 12(31)
+	mr 3, 31
+	lwz 30, 24(1)
+	stw 10, 0(31)
+	stw 11, 4(31)
+	mtlr 0
+	lwz 31, 28(1)
+	addi 1, 0x20
+	blr
+
+.ret0:
+	lis 4, uint128_0@ha
+	mr 3, 31
+	la 4, uint128_0@l(4)
+	bl uint128_t_constructor
+
+.return:
+	lwz 0, 36(1)
+	mr 3, 31
+	lwz 29, 20(1)
+	lwz 30, 24(1)
+	lwz 31, 28(1)
+	addi 1, 0x20
+	mtlr 0
+	blr
+
+.check3F:
+	cmplwi 7, 11, 0x3F
+	bgt 7, .doAdds
+
+	subfic 9, 11, 0x40
+	lwz 3, 0(4)
+	lwz 0, 4(4)
+	addic. 8, 9, -0x20
+	blt 0, .doShifts2
+
+	slw 6, 0, 8
+	li 7, 0
+
+.check20:
+	addic. 5, 11, -0x20
+	blt 0, .doShifts
+
+	lwz 4, 8(4)
+	li 8, 0
+	srw 9, 4, 5
+
+.check0:
+	cmpwi 7, 5, 0
+	addc 9, 7
+	adde 8, 6
+	blt 7, .doShifts3
+
+	srw 5, 3, 5
+	li 10, 0
+	stw 5, 4(31)
+	stw 10, 0(31)
+
+.doLoadsStores2:
+	lwz 0, 36(1)
+	stw 0, 8(31)
+	mr 3, 31
+	lwz 29, 20(1)
+	stw 9, 12(31)
+	lwz 30, 24(1)
+	lwz 31, 28(1)
+	addi 1, 0x20
+	mtlr 0
+	blr
+
+.doHalf:
+	li 10, 0
+	stw 10, 0(3)
+	stw 10, 4(3)
+
+	lwz 10, 0(4)
+	lwz 11, 4(4)
+	stw 10, 8(3)
+	stw 11, 12(3)
+	b .l16
+
+.doConstruct:
+	bl uint128_t_constructor
+	b .return
+
+.doShifts2:
+	srwi 8, 0, 1
+	subfic 5, 9, 0x1F
+	srw 8, 5
+	slw 6, 3, 9
+	or 6, 8, 6
+	slw 7, 0, 9
+	b .check20
+
+.doShifts3:
+	slwi 7, 3, 1
+	subfic 10, 11, 0x1F
+	slw 10, 7, 10
+	srw 0, 11
+	or 0, 10
+	srw 3, 11
+	stw 0, 4(31)
+	stw 3, 0(31)
+	b .doLoadsStores2
+
+.doShifts:
+	lwz 12, 8(4)
+	lwz 30, 12(4)
+	subfic 4, 11, 0x1F
+	slwi 29, 12, 1
+	srw 9, 30, 11
+	slw 4, 29, 4
+	srw 8, 12, 11
+	or 9, 4, 9
+	b .check0
+
+.doShifts4:
+	lwz 9, 4(4)
+	subfic 7, 10, 0x1F
+	slwi 6, 8, 1
+	slw 7, 6, 7
+	srw 9, 10
+	srw 10, 8, 10
+	or 9, 7, 9
+	b .doLoadsStores
 
 
 
