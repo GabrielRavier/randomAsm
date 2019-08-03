467b1dc4f49736db09e9400c813de29bc48ca9a0
Gabriel Ravier <gabravier@gmail.com>
2019-08-03 21:54:11 +0200

    Forgot [xy][hl] stuff in ARM/divf.asm
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- ARM/divf.asm
+++ ARM/divf.asm
@@ -5,8 +5,8 @@ __aeabi_ddiv:
 
 	mov ip, #0xFF
 	orr ip, #0x700
-	ands r3, ip, xh, lsr #20
-	andsne r5, ip, yh, lsr #20
+	ands r3, ip, r0, lsr #20
+	andsne r5, ip, r2, lsr #20
 
 	.irp reg, r4, r5
 		teqne \reg, ip
@@ -15,67 +15,67 @@ __aeabi_ddiv:
 
 	sub r4, r5
 
-	eor lr, xh, yh
+	eor lr, r0, r2
 
-	orrs r5, yl, yh, lsl #12
-	mov xh, lsl #12
+	orrs r5, r3, r2, lsl #12
+	mov r0, lsl #12
 	beq .l1
 
-	mov yh, lsl #12
+	mov r2, lsl #12
 	mov r5, #0x10000000
-	orr yh, r5, yh, lsr #4
-	orr yh, yl, lsr #24
-	mov yl, lsl #8
-	orr r5, xh, lsr #4
-	orr r5, xl, lsr #24
-	mov r6, xl, lsl #8
+	orr r2, r5, r2, lsr #4
+	orr r2, r3, lsr #24
+	mov r3, lsl #8
+	orr r5, r0, lsr #4
+	orr r5, r1, lsr #24
+	mov r6, r1, lsl #8
 
-	and xh, lr, #0x80000000
+	and r0, lr, #0x80000000
 
-	cmp r5, yh
-	cmpeq r6, yl
+	cmp r5, r2
+	cmpeq r6, r3
 	adc r4, #253
 	add r4, #0x300
 	bcs .l12
 
-	movs yh, lsr #1
-	mov yl, rrx
+	movs r2, lsr #1
+	mov r3, rrx
 
 .l12:
-	subs r6, yl
-	sbc r5, yh
-	movs yh, lsr #1
-	mov yl, rrx
-	mov xl, #0x100000
+	subs r6, r3
+	sbc r5, r2
+	movs r2, lsr #1
+	mov r3, rrx
+	mov r1, #0x100000
 	mov ip, #0x80000
 
 .l13:
-	subs lr, r6, yl
-	sbcs lr, r5, yh
-	subcs r6, yl
+	subs lr, r6, r3
+	sbcs lr, r5, r2
+	subcs r6, r3
 	movcs r5, lr
-	orrcs xl, ip
-	movs yh, lsr #1
-	mov yl, rrx
-	subs lr, r6, yl
-	sbcs lr, r5, yh
-	subcs r6, yl
+	orrcs r1, ip
+	movs r2, lsr #1
+	mov r3, rrx
+	subs lr, r6, r3
+	sbcs lr, r5, r2
+	subcs r6, r3
 	movcs r5, lr
-	orrcs xl, ip, lsr #1
-	movs yh, lsr #1
-	mov yl, rrx
-	subs lr, r6, yl
-	sbcs lr, r5, yh
-	subcs r6, yl
+	orrcs r1, ip, lsr #1
+	movs r2, lsr #1
+	mov r3, rrx
+	subs lr, r6, r3
+	sbcs lr, r5, r2
+	subcs r6, r3
 	movcs r5, lr
-	orrcs xl, ip, lsr #2
-	movs yh, lsr #1
-	mov yl, rrx
-	subs lr, r6, yl
-	sbcs lr, r5, yh
-	subcs r6, yl
+	orrcs r1, ip, lsr #2
+	movs r2, lsr #1
+	mov r3, rrx
+	subs lr, r6, r3
+	sbcs lr, r5, r2
+	subcs r6, r3
 	movcs r5, lr
-	orrcs xl, ip, lsr #3
+	orrcs r1, ip, lsr #3
 
 	orrs lr, r5, r6
 	beq .l2
@@ -83,47 +83,47 @@ __aeabi_ddiv:
 	mov r5, lsl #4
 	orr r5, r6, lsr #28
 	mov r6, lsl #4
-	mov yh, lsl #3
-	orr yh, yl, lsr #29
-	mov yl, lsl #3
+	mov r2, lsl #3
+	orr r2, r3, lsr #29
+	mov r3, lsl #3
 	movs ip, lsr #4
 	bne .l13
 
-	tst xh, #0x100000
+	tst r0, #0x100000
 	bne .l3
 
-	orr xh, xl
-	mov xl, #0
+	orr r0, r1
+	mov r1, #0
 	mov ip, #0x80000000
 	b .l13
 
 .l2:
-	tst xh, #0x100000
-	orreq xh, xl
-	moveq xl, #0
+	tst r0, #0x100000
+	orreq r0, r1
+	moveq r1, #0
 
 .l3:
 	subs ip, r4, #253
 	cmphi ip, #0x700
 	bhi __aeabi_dmul.u
 
-	subs ip, r5, yh
-	subseq ip, r6, yl
-	movseq ip, xl, lsr #1
+	subs ip, r5, r2
+	subseq ip, r6, r3
+	movseq ip, r1, lsr #1
 
-	adcs xl, #0
-	adc xh, r4, lsl #20
+	adcs r1, #0
+	adc r0, r4, lsl #20
 	pop {r4, r5, r6, pc}
 
 .l1:
 	and lr, #0x80000000
-	orr xh, lr, xh, lsr #12
+	orr r0, lr, r0, lsr #12
 	adds r4, ip, lsr #1
 	rsbsgt r5, r4, ip
-	orrgt xh, r4, lsl #20
+	orrgt r0, r4, lsl #20
 	popgt {r4, r5, r6, pc}
 
-	orr xh, #0x100000
+	orr r0, #0x100000
 	mov lr, #0
 	subs r4, #1
 	b __aeabi_dmul.u
@@ -133,7 +133,7 @@ __aeabi_ddiv:
 	b __aeabi_dmul.u
 
 .s:
-	and r5, ip, yh, lsr #20
+	and r5, ip, r2, lsr #20
 	teq r4, ip
 	teqeq r5, ip
 	beq __aeabi_dmul.n
@@ -141,35 +141,35 @@ __aeabi_ddiv:
 	teq r4, ip
 	bne .l14
 
-	orrs r4, xl, xh, lsl #12
+	orrs r4, r1, r0, lsl #12
 	bne __aeabi_dmul.n
 
 	teq r5, ip
 	bne __aeabi_dmul.i
 
-	mov xl, yl
-	mov xh, yh
+	mov r1, r3
+	mov r0, r2
 	b __aeabi_dmul.n
 
 .l14:
 	teq r5, ip
 	bne .l22
 
-	orrs r5, yl, yh, lsl #12
+	orrs r5, r3, r2, lsl #12
 	beq __aeabi_dmul.z
 
-	mov xl, yl
-	mov xh, yh
+	mov r1, r3
+	mov r0, r2
 
 .l22:
-	orrs r6, xl, xh, lsl #1
-	orrsne r6, yl, yh, lsl #1
+	orrs r6, r1, r0, lsl #1
+	orrsne r6, r3, r2, lsl #1
 	bne __aeabi_dmul.d
 
-	orrs r4, xl, xh, lsl #1
+	orrs r4, r1, r0, lsl #1
 	bne __aeabi_dmul.i
 
-	orrs r5, yl, yh, lsl #1
+	orrs r5, r3, r2, lsl #1
 	bne __aeabi_dmul.z
 	b __aeabi_dmul.n
 
